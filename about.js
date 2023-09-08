96686bb0238cc2d256574de88194ce4856a2d4cc
Cherry-Coded <136689572+Cherry-Coded@users.noreply.github.com>
2023-09-08 13:22:30 -0700

    Update about.js

--- about.js
+++ about.js
@@ -1,25 +1,23 @@
-<script>
-    // Function to handle form submission
-    function handleSubmit(event) {
-        event.preventDefault();
-        
-        // Get the selected value (yes or no)
-        const alignmentValue = document.querySelector('input[name="alignment"]:checked');
+// Function to handle form submission
+function handleSubmit(event) {
+  // Get the selected value (yes or no)
+  const alignmentValue = document.querySelector(
+    'input[name="alignment"]:checked'
+  );
 
-        if (!alignmentValue) {
-            alert("Please select 'Yes' or 'No' to align with our mission.");
-            return;
-        }
+  if (!alignmentValue) {
+    event.preventDefault();
+    alert("Please select 'Yes' or 'No' to align with our mission.");
+  }
 
-        // You can perform further actions based on the selected value here.
-        if (alignmentValue.value === "yes") {
-            alert("Thank you for wanting to help save our oceans!");
-        } else if (alignmentValue.value === "no") {
-     alert("We appreciate your honesty.");
-        }
-    }
+  // You can perform further actions based on the selected value here.
+  if (alignmentValue.value === "yes") {
+    alert("Thank you for wanting to help save our oceans!");
+  } else if (alignmentValue.value === "no") {
+    alert("We appreciate your honesty.");
+  }
+}
 
-    // Add an event listener to the form for submission
-    const missionForm = document.getElementById("mission-form");
-    missionForm.addEventListener("submit", handleSubmit);
-</script>
+// Add an event listener to the form for submission
+const missionForm = document.getElementById("mission-form");
+missionForm.addEventListener("submit", handleSubmit);
